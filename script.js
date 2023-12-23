85f8b27ff81e5acb4b2187c28c7f0c3544df2c74
NikhilDighe9975 <nikhildighe32@gmail.com>
2023-12-23 09:30:55 +0530

    changes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer","Freelancer"],
+        strings: ["Full Stack Developer","Mobileapp developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
